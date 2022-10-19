93775d2065c5e94eba81a4a1b5b1f93650b96c0c
binguniverseca <109619587+binguniverseca@users.noreply.github.com>
2022-10-19 09:12:24 -0400

    Add files via upload

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,16 @@
 'use strict'
 
-import {app, BrowserWindow, ipcMain} from 'electron'
-// related to update
+import {app, BrowserWindow, ipcMain, Menu, Tray, shell} from 'electron'
+
 import {autoUpdater} from 'electron-updater'
 
+
+let appTray = null
+
+let trayClose = false
 /**
  * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ 
  */
 if (process.env.NODE_ENV !== 'development') {
     global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
@@ -22,17 +26,63 @@ function createWindow() {
      * Initial window options
      */
     mainWindow = new BrowserWindow({
-        height: 563,
+        height: 840,
         useContentSize: true,
-        width: 1000
+        width: 1080
     })
 
     mainWindow.loadURL(winURL)
 
-    mainWindow.webContents.openDevTools()
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools()
+    }
+    mainWindow.on('close', (event) => {
+        if (!trayClose) {
+            // 最小化
+            mainWindow.hide()
+            event.preventDefault()
+        }
+    })
 
     mainWindow.on('closed', () => {
         mainWindow = null
+    })
+
+   
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    const trayMenuTemplate = [
+        {
+            label: 'About',
+            click: function () {
+           
+                shell.openExternal('http://www.google.com')
+            }
+        },
+        {
+            label: 'Exit',
+            click: function () {
+                
+                trayClose = true
+                app.quit()
+            }
+        }
+    ]
+   
+    const path = require('path')
+    const iconPath = path.join(__dirname, '/static/icon2.ico')
+    appTray = new Tray(iconPath)
+   
+    const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
+  
+    appTray.setToolTip('')
+    
+    appTray.setContextMenu(contextMenu)
+   
+    appTray.on('click', () => {
+        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
     })
 }
 
@@ -50,4 +100,69 @@ app.on('activate', () => {
     }
 })
 
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * 
 
+function sendUpdateMessage(text) {
+    mainWindow.webContents.send('message', text)
+}
+
+
+let message = { // eslint-disable-line no-unused-vars
+    error: 'An error occured during updating',
+    checking: 'Updating......',
+    updateAva: 'Downloading new version......',
+    updateNotAva: 'This is the latest version'
+}
+
+const os = require('os') // eslint-disable-line no-unused-vars
+
+// An error occured during updating
+autoUpdater.on('error', (err) => {
+    sendUpdateMessage('error')
+})
+// Updating
+autoUpdater.on('checking-for-update', () => {
+    sendUpdateMessage('checking')
+})
+// auto updator, downloading
+autoUpdater.autoDownload = false
+autoUpdater.on('update-available', (info) => {
+    sendUpdateMessage('updateAva')
+})
+// update not available
+autoUpdater.on('update-not-available', (info) => {
+    sendUpdateMessage('updateNotAva')
+})
+// download progress
+autoUpdater.on('download-progress', (progressObj) => {
+    mainWindow.webContents.send('downloadProgress', progressObj)
+})
+/**
+ * event Event
+ * releaseNotes String 
+ * releaseName String 
+ * releaseDate Date 
+ * updateUrl String 
+ */
+autoUpdater.on('update-downloaded', (info) => {
+    ipcMain.on('isUpdateNow', (e, arg) => {
+        // some code here to handle event
+        autoUpdater.quitAndInstall()
+    })
+    mainWindow.webContents.send('isUpdateNow')
+})
+
+ipcMain.on('checkForUpdate', () => {
+    
+    autoUpdater.checkForUpdates()
+})
+
+ipcMain.on('downloadUpdate', () => {
+   
+    autoUpdater.downloadUpdate()
+})
